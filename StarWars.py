29bdd8a550cd8c0c9cdde8c24130641924bf0501
jedlikKA <kormondialex@gmail.com>
2021-02-24 13:32:35 +0100

    Irányitás kész

--- StarWars.py
+++ StarWars.py
@@ -2,15 +2,59 @@ import turtle
 import random
 import time
 
+def fel():
+    ypozicio=urhajo.ycor()
+    ypozicio+=10
+    urhajo.sety(ypozicio)
+
+
+def le():
+    ypozicio=urhajo.ycor()
+    ypozicio-=10
+    urhajo.sety(ypozicio)
+
+
+def jobbra():
+    xpozicio=urhajo.xcor()
+    xpozicio+=10
+    urhajo.setx(xpozicio)
+
+
+def balra():
+    xpozicio=urhajo.xcor()
+    xpozicio-=10
+    urhajo.setx(xpozicio)
+
+
 space = turtle.Screen()
 space.setup(width=800, height=600)
 space.bgpic("Background.png")
 space.addshape("sprite.gif")
 
+space.tracer(0)
+space.listen()
+space.onkey(fel, "Up")
+space.onkey(le, "Down")
+space.onkey(jobbra, "Right")
+space.onkey(balra, "Left")
+
 urhajo = turtle.Turtle()
 urhajo.shape("sprite.gif")
+urhajo.penup()
 
 while True:
     space.update()
     time.sleep(0.2)
+
+    if urhajo.ycor() > 300:
+        urhajo.sety(-300)
+
+    if urhajo.xcor() > 400:
+        urhajo.setx(-400)
+
+    if urhajo.ycor() < -300:
+        urhajo.sety(300)
+
+    if urhajo.xcor() < -400:
+        urhajo.setx(400)
 
