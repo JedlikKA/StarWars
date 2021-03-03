c5537027daf5f75f63aa31dbfa6657311ece5390
jedlikKA <kormondialex@gmail.com>
2021-03-03 12:40:20 +0100

    Meteor mozgás

--- StarWars.py
+++ StarWars.py
@@ -3,58 +3,69 @@ import random
 import time
 
 def fel():
-    ypozicio=urhajo.ycor()
-    ypozicio+=10
-    urhajo.sety(ypozicio)
+    ypozizcio= urhajo.ycor()
+    ypozizcio += 10
+    urhajo.sety(ypozizcio)
 
 
 def le():
-    ypozicio=urhajo.ycor()
-    ypozicio-=10
-    urhajo.sety(ypozicio)
+    ypozizcio= urhajo.ycor()
+    ypozizcio -= 10
+    urhajo.sety(ypozizcio)
 
 
 def jobbra():
-    xpozicio=urhajo.xcor()
-    xpozicio+=10
-    urhajo.setx(xpozicio)
+    xpozizcio= urhajo.xcor()
+    xpozizcio += 10
+    urhajo.setx(xpozizcio)
 
 
 def balra():
-    xpozicio=urhajo.xcor()
-    xpozicio-=10
-    urhajo.setx(xpozicio)
-
+    xpozizcio= urhajo.xcor()
+    xpozizcio -= 10
+    urhajo.setx(xpozizcio)
 
 space = turtle.Screen()
 space.setup(width=800, height=600)
 space.bgpic("Background.png")
 space.addshape("sprite.gif")
-
+space.addshape("meteor2.gif")
 space.tracer(0)
 space.listen()
-space.onkey(fel, "Up")
-space.onkey(le, "Down")
-space.onkey(jobbra, "Right")
-space.onkey(balra, "Left")
+space.onkeypress(balra,"Left")
+space.onkeypress(jobbra,"Right")
+space.onkeypress(fel,"Up")
+space.onkeypress(le,"Down")
+
 
 urhajo = turtle.Turtle()
 urhajo.shape("sprite.gif")
 urhajo.penup()
 
+meteorjobb = turtle.Turtle()
+meteorjobb.shape("meteor2.gif")
+meteorjobb.penup()
+
+x = random.randint(130, 380)
+y = random.randint(-280, 280)
+meteorjobb.goto(x, y)
+while meteorjobb.xcor()>-400:
+    space.update()
+    time.sleep(0.2)
+    meteormozgas = meteorjobb.xcor()
+    meteormozgas -= 10
+    meteorjobb.setx(meteormozgas)
+
 while True:
+
     space.update()
     time.sleep(0.2)
 
-    if urhajo.ycor() > 300:
+    if urhajo.ycor()>300:
         urhajo.sety(-300)
-
-    if urhajo.xcor() > 400:
+    if urhajo.xcor()>400:
         urhajo.setx(-400)
-
-    if urhajo.ycor() < -300:
-        urhajo.sety(300)
-
-    if urhajo.xcor() < -400:
+    if urhajo.xcor()<-400:
         urhajo.setx(400)
-
+    if urhajo.ycor()<-300:
+        urhajo.sety(300)
