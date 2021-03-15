eb08bb0ef53d5300d37724038a6b126c35fe6b49
jedlikKA <kormondialex@gmail.com>
2021-03-15 22:01:45 +0100

    2.-3. Feladat kész

--- StarWars.py
+++ StarWars.py
@@ -1,29 +1,39 @@
 import turtle
 import random
 import time
+from random import randint
+from random import seed
+
+for _ in range(1):
+    value = randint(-300, 300)
+
 
 def fel():
-    ypozizcio= urhajo.ycor()
-    ypozizcio += 10
-    urhajo.sety(ypozizcio)
+    ypozicio = urhajo.ycor()
+    ypozicio += 20
+    urhajo.sety(ypozicio)
 
 
 def le():
-    ypozizcio= urhajo.ycor()
-    ypozizcio -= 10
-    urhajo.sety(ypozizcio)
+    ypozicio = urhajo.ycor()
+    ypozicio -= 20
+    urhajo.sety(ypozicio)
 
 
 def jobbra():
-    xpozizcio= urhajo.xcor()
-    xpozizcio += 10
-    urhajo.setx(xpozizcio)
+    xpozicio = urhajo.xcor()
+    xpozicio += 20
+    urhajo.setx(xpozicio)
 
 
 def balra():
-    xpozizcio= urhajo.xcor()
-    xpozizcio -= 10
-    urhajo.setx(xpozizcio)
+    xpozicio = urhajo.xcor()
+    xpozicio -= 20
+    urhajo.setx(xpozicio)
+
+
+kijelzo = turtle.Turtle()
+kijelzo.hideturtle()
 
 space = turtle.Screen()
 space.setup(width=800, height=600)
@@ -32,40 +42,51 @@ space.addshape("sprite.gif")
 space.addshape("meteor2.gif")
 space.tracer(0)
 space.listen()
-space.onkeypress(balra,"Left")
-space.onkeypress(jobbra,"Right")
-space.onkeypress(fel,"Up")
-space.onkeypress(le,"Down")
-
+space.onkeypress(fel, "Up")
+space.onkeypress(le, "Down")
+space.onkeypress(balra, "Left")
+space.onkeypress(jobbra, "Right")
 
 urhajo = turtle.Turtle()
 urhajo.shape("sprite.gif")
 urhajo.penup()
 
-meteorjobb = turtle.Turtle()
-meteorjobb.shape("meteor2.gif")
-meteorjobb.penup()
+meteor_jobbrol = turtle.Turtle()
+meteor_jobbrol.shape("meteor2.gif")
+meteor_jobbrol.penup()
 
-x = random.randint(130, 380)
-y = random.randint(-280, 280)
-meteorjobb.goto(x, y)
-while meteorjobb.xcor()>-400:
+meteor_jobbrol.setx(400)
+meteor_jobbrol.sety(value)
+
+szamlalo = 0
+
+while meteor_jobbrol.xcor() > -400:
     space.update()
-    time.sleep(0.2)
-    meteormozgas = meteorjobb.xcor()
-    meteormozgas -= 10
-    meteorjobb.setx(meteormozgas)
+    time.sleep(0.1)
+    meteor_mozgas = meteor_jobbrol.xcor()
+    meteor_mozgas -= 50
+    meteor_jobbrol.setx(meteor_mozgas)
 
-while True:
+while meteor_jobbrol.xcor() <= -400:
+    szamlalo += 1
+    kijelzo.clear()
+    kijelzo.write(szamlalo, align="center", font=("Arial", 30, "bold"))
+    for _ in range(1):
+        value = randint(-300, 300)
+    meteor_jobbrol.setx(400)
+    meteor_jobbrol.sety(value)
+    while meteor_jobbrol.xcor() > -400:
+        space.update()
+        time.sleep(0.1)
+        meteor_mozgas = meteor_jobbrol.xcor()
+        meteor_mozgas -= 50
+        meteor_jobbrol.setx(meteor_mozgas)
 
-    space.update()
-    time.sleep(0.2)
-
-    if urhajo.ycor()>300:
-        urhajo.sety(-300)
-    if urhajo.xcor()>400:
-        urhajo.setx(-400)
-    if urhajo.xcor()<-400:
-        urhajo.setx(400)
-    if urhajo.ycor()<-300:
-        urhajo.sety(300)
+        if urhajo.ycor()>300:
+            urhajo.sety(-300)
+        if urhajo.xcor()>400:
+            urhajo.setx(-400)
+        if urhajo.xcor()<-400:
+            urhajo.setx(400)
+        if urhajo.ycor()<-300:
+            urhajo.sety(300)
