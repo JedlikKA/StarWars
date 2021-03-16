1e5474ac56d76d2fa92748cab85abf635e7162f9
jedlikKA <kormondialex@gmail.com>
2021-03-16 20:44:35 +0100

    Kész a játék 1.0 verzió

--- StarWars.py
+++ StarWars.py
@@ -40,6 +40,7 @@ space.setup(width=800, height=600)
 space.bgpic("Background.png")
 space.addshape("sprite.gif")
 space.addshape("meteor2.gif")
+space.addshape("meteor1.gif")
 space.tracer(0)
 space.listen()
 space.onkeypress(fel, "Up")
@@ -51,45 +52,68 @@ urhajo = turtle.Turtle()
 urhajo.shape("sprite.gif")
 urhajo.penup()
 
-meteor_jobbrol = turtle.Turtle()
-meteor_jobbrol.shape("meteor2.gif")
-meteor_jobbrol.penup()
+meteor = turtle.Turtle()
+meteor.penup()
+shapes = ["meteor2.gif","meteor1.gif"]
+meteor.shape(random.choice(shapes))
+meteor.setx(400)
+meteor.sety(random.randint(-270,270))
 
-meteor_jobbrol.setx(400)
-meteor_jobbrol.sety(value)
 
-szamlalo = 0
+pontok = 0
+elet = 0
+pen=turtle.Turtle()
+pen.hideturtle()
 
-while meteor_jobbrol.xcor() > -400:
+
+while True:
+
     space.update()
     time.sleep(0.1)
-    meteor_mozgas = meteor_jobbrol.xcor()
-    meteor_mozgas -= 50
-    meteor_jobbrol.setx(meteor_mozgas)
 
-while meteor_jobbrol.xcor() <= -400:
-    szamlalo += 1
-    kijelzo.clear()
-    kijelzo.color("white")
-    kijelzo.penup()
-    kijelzo.goto(0,260) 
-    kijelzo.write(f"Pontjaid: {szamlalo}", align="center", font=("Arial", 30, "bold"))
-    for _ in range(1):
-        value = randint(-300, 300)
-    meteor_jobbrol.setx(400)
-    meteor_jobbrol.sety(value)
-    while meteor_jobbrol.xcor() > -400:
-        space.update()
-        time.sleep(0.1)
-        meteor_mozgas = meteor_jobbrol.xcor()
-        meteor_mozgas -= 50
-        meteor_jobbrol.setx(meteor_mozgas)
+    if urhajo.ycor() > 300:
+        urhajo.sety(-300)
+    if urhajo.ycor() < -300:
+        urhajo.sety(300)
+    if urhajo.xcor() > 400:
+        urhajo.setx(-400)
+    if urhajo.xcor() < -400:
+        urhajo.setx(400)
 
-        if urhajo.ycor()>300:
-            urhajo.sety(-300)
-        if urhajo.xcor()>400:
-            urhajo.setx(-400)
-        if urhajo.xcor()<-400:
-            urhajo.setx(400)
-        if urhajo.ycor()<-300:
-            urhajo.sety(300)
+    if meteor.xcor() < -400 or meteor.ycor() < -300 or meteor.ycor() > 300:
+        meteor.shape(random.choice(shapes))
+        meteor.setx(400)
+        meteor.sety(random.randint(-270,270))
+
+        pontok += 1
+        pen.clear()
+        pen.color("white")
+        pen.penup()
+        pen.goto(-320,260) 
+        pen.write(f"Pontjaid: {pontok}", align="center", font=("Arial", 20, "bold"))
+
+
+    if urhajo.distance(meteor.xcor(), meteor.ycor()) < 70:
+        meteor.shape(random.choice(shapes))
+        meteor.setx(400)
+        meteor.sety(random.randint(-270,270))
+        
+        elet += 1 
+        pen.clear()
+        pen.color("red")
+        pen.penup()
+        pen.goto(0,260) 
+        pen.write(f"Találat: {elet}", align="center", font=("Arial",20,"bold"))
+
+
+
+    if meteor.shape() == "meteor2.gif":
+        meteor.setx(meteor.xcor()-15)
+    elif meteor.shape() == "meteor1.gif":
+        meteor.setx(meteor.xcor()-15)
+    else:
+        meteor.setx(meteor.xcor()+15)
+    
+    if elet == 3:
+        space.clear()                                                                  
+        kijelzo.write("MEGHALTÁL!", align="center", font=("Arial", 30, "bold"))
